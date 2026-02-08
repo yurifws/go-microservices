944e3d765d331ca436686bd1785b7c5b5321abac
yurifws <yurifws@gmail.com>
2026-02-07 21:35:06 -0300

    <feat GMS-015> creating read, write and error JSON to help

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -51,4 +51,20 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	if err != nil {
 		return err
 	}
+
+	return nil
+}
+
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return app.writeJSON(w, statusCode, payload)
 }
