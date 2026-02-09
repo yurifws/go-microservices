b49d9da7911cfceafab086937b441f9444a8b454
yurifws <yurifws@gmail.com>
2026-02-09 01:08:31 -0300

    Add .gitignore and remove db-data from version control

--- broker-service/cmd/api/handler.go
+++ broker-service/cmd/api/handler.go
@@ -9,7 +9,7 @@ import (
 
 type ResquestPayload struct {
 	Action string `json:"action"`
-	Auth AuthPayload `json:"auth, omitempty"`
+	Auth AuthPayload `json:"auth,omitempty"`
 }
 
 type AuthPayload struct {
@@ -45,10 +45,10 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
-	//create some json we'll send to the auth microservice
+	// create some json we'll send to the auth microservice
 	jsonData, _ := json.MarshalIndent(a, "", "\t")
 
-	//call the service
+	// call the service
 	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
 	if err != nil {
 		app.errorJSON(w, err)
@@ -63,7 +63,7 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 	}
 	defer response.Body.Close()
 
-	//make sure we get back the correct status code
+	// make sure we get back the correct status code
 	if response.StatusCode == http.StatusUnauthorized {
 		app.errorJSON(w, errors.New("invalid credentials"))
 		return
@@ -72,10 +72,10 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 		return
 	}
 
-	//create a variable we'll read response.Body into
+	// create a varabiel we'll read response.Body into
 	var jsonFromService jsonResponse
 
-	//decode the json from the auth service
+	// decode the json from the auth service
 	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
 	if err != nil {
 		app.errorJSON(w, err)
@@ -89,7 +89,7 @@ func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
 
 	var payload jsonResponse
 	payload.Error = false
-	payload.Message = "Authenticated"
+	payload.Message = "Authenticated!"
 	payload.Data = jsonFromService.Data
 
 	app.writeJSON(w, http.StatusAccepted, payload)
