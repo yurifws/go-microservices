c0d2039e44e65d1c5b00375e47aab4bd8c703058
yurifws <yurifws@gmail.com>
2026-02-10 01:07:32 -0300

    <feat GMS-029> starting a logger-service

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	mongoURL = "mongodb://mongo:27017"
+	gRpcPort = "50001"
+)
+
+var client *mongo.Client
+
+type Config struct {
+
+}
+
+func main() {
+	//connect to mongo
+	mongoClient, err := conectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func conectToMongo() (*mongo.Client, error) {
+	//create connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	//connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connecting:", err)
+	}
+
+	return c, nil
+}
