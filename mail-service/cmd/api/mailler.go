c5d796b9621b359a908dd017c0eb9ec25701123a
yurifws <yurifws@gmail.com>
2026-02-18 00:05:03 -0300

    <feat GMS-41> building the logic to send email

--- mail-service/cmd/api/mailler.go
+++ mail-service/cmd/api/mailler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"html/template"
+	"time"
 
 	"github.com/vanng822/go-premailer/premailer"
 	mail "github.com/xhit/go-simple-mail/v2"
@@ -55,7 +56,39 @@ func (m *Mail) SendSMTPMessage(msg Message) error {
 	}
 
 	server := mail.NewSMTPClient()
+	server.Host = m.Host
+	server.Port = m.Port
+	server.Username = m.Username
+	server.Password = m.Password
+	server.Encryption = m.getEncryption(m.Encryption)
+	server.KeepAlive = false
+	server.ConnectTimeout = 10 * time.Second
+	server.SendTimeout = 10 * time.Second
 
+	smtpClient, err := server.Connect()
+	if err != nil {
+		return err
+	}
+
+	email := mail.NewMSG()
+	email.SetFrom(msg.From)
+	email.AddTo(msg.To)
+	email.SetSubject(msg.Subject)
+	email.SetBody(mail.TextPlain, plainMessage)
+	email.AddAlternative(mail.TextHTML, formattedMessage)
+
+	if len(msg.Attachments) > 0 {
+		for _, x := range msg.Attachments {
+			email.AddAttachment(x)
+		}
+	}
+
+	err = email.Send(smtpClient)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (m *Mail) buildHTMLMessage(msg Message) (string, error) {
@@ -117,4 +150,17 @@ func (m *Mail) inlineCSS(s string) (string, error) {
 	}
 
 	return html, nil
+}
+
+func (m *Mail) getEncryption(s string) mail.Encryption {
+	switch s {
+	case "tls":
+		return mail.EncryptionSTARTTLS
+	case "ssl":
+		return  mail.EncryptionSSL
+	case "none", "":
+		return mail.EncryptionNone
+	default:
+		return mail.EncryptionSTARTTLS
+	}
 }
